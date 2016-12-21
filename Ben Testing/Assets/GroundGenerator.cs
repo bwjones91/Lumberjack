810c80bfad4e33ab714d0171042aec99e9675cb7
GatorSutton <garrettsuttonee@gmail.com>
2016-12-21 00:56:11 -0500

    LevelGeneration Working
    Needs polishing but ground will duplicate ifyou run to the right

---
+++ Ben Testing/Assets/GroundGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGenerator : MonoBehaviour
+{
+
+    public GameObject theGroundSection;
+    public GameObject generationPointRight;
+    public GameObject geneartionPointLeft;
+
+   
+    private float groundWidth;
+    private 
+
+
+    // Use this for initialization
+    void Start()
+    {
+        groundWidth = theGroundSection.GetComponent<Collider2D>().bounds.size.x;
+        print(groundWidth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > generationPointRight.transform.position.x)
+        {
+            Instantiate(theGroundSection, generationPointRight.transform.position, generationPointRight.transform.rotation);
+            generationPointRight.transform.position = new Vector3(generationPointRight.transform.position.x + groundWidth, generationPointRight.transform.position.y, generationPointRight.transform.position.z);
+            
+        }
+
+    }
+}
